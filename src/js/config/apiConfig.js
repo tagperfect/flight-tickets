114d1c2bed50987d3144cded44417f6ec97014c2
tagperfect <andrei@tagperfect.eu>
2022-12-09 15:36:55 +0200

    app build

--- src/js/config/apiConfig.js
+++ src/js/config/apiConfig.js
@@ -5,12 +5,12 @@ const config = {
   // token: 'e3d5607a29a777706e816c35b5591ee9',
   // maker: 399405,
   headers: {
-    'Content-Type': 'application/json;charset=utf-8',
-    'Access-Control-Allow-Origin': '*',
+    // 'Content-Type': 'application/json;charset=utf-8',
+    // 'Access-Control-Allow-Origin': '*',
     // 'User-Agent': 'PostmanRuntime/7.29.2',
-    'Accept-Encoding': 'gzip, deflate, br',
-    Accept: '*/*',
-    "Connection": 'keep-alive',
+    // 'Accept-Encoding': 'gzip, deflate, br',
+    // Accept: '*/*',
+    // "Connection": 'keep-alive',
     "X-Access-Token": 'e3d5607a29a777706e816c35b5591ee9',
   }
 };
