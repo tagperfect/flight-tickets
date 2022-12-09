5c0bcaca0bde3802460b1c247a9c12f9bf5653d3
tagperfect <andrei@tagperfect.eu>
2022-12-09 16:21:17 +0200

    app build

--- src/js/config/apiConfig.js
+++ src/js/config/apiConfig.js
@@ -1,6 +1,6 @@
 const config = {
   // url: 'https://update.rhm.fi'
-  url: 'https://api.travelpayouts.com',
+  url: 'http://api.travelpayouts.com',
   // url: 'https://aviasales-api.herokuapp.com'
   token: 'e3d5607a29a777706e816c35b5591ee9',
   // maker: 399405,
