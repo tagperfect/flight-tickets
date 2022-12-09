263eed6b8fdb1a0ad2c3c084c14a35b711f6dd24
tagperfect <andrei@tagperfect.eu>
2022-12-09 17:16:12 +0200

    app build

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -12,37 +12,16 @@ class Api {
     this.url = config.url;
     this.token = config.token;
     this.headers = config.headers;
-    this.config = {
-      method: 'get',
-      url: 'https://api.travelpayouts.com/data/en-GB/countries.json',
-      headers: {
-        'X-Access-Token': 'e3d5607a29a777706e816c35b5591ee9'
-      }
-    };
   }
   async countries() {
     try {
-      // const responce = await axios(this.config);
-      const responce = await axios.get(
-        `${this.url}/data/en-GB/countries.json`,
-        {
-          headers: this.headers
-        }
-      );
-      // {
-      // const responce = await axios.get(`https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json`, {
-      //   headers: {
-      //   'Content-Type': 'application/json;charset=utf-8',
-      // 'Access-Control-Allow-Origin': '*',
-      // 'User-Agent': 'PostmanRuntime/7.29.2',
-      //   'Accept-Encoding': 'gzip, deflate, br',
-      //   Accept: '*/*',
-      //   "Connection": 'keep-alive',
-      //   "X-Access-Token": this.token,
-
-      // }
-      // }
+      // const responce = await axios.get(
+      //   `${this.url}/data/en-GB/countries.json`,
+      //   {
+      //     headers: this.headers
+      //   }
       // );
+      const responce = await axios.get(`https://api.travelpayouts.com/aviasales_resources/v3/countries.json`);
       return responce.data;
     } catch (err) {
       console.log(err);
